6ab31f60c7b63b4c3fde91fd47f363ae20fdcbf4
kimonvl <kimonvloutis@gmail.com>
2026-04-13 02:21:57 +0300

    feat: add create question end point

--- src/main/java/com/example/rewarded_questions_app/api/QuestionnaireRestController.java
+++ src/main/java/com/example/rewarded_questions_app/api/QuestionnaireRestController.java
@@ -1,11 +1,14 @@
 package com.example.rewarded_questions_app.api;
 
+import com.example.rewarded_questions_app.dto.CreateQuestionRequest;
 import com.example.rewarded_questions_app.dto.CreateQuestionnaireRequest;
 import com.example.rewarded_questions_app.dto.GenericResponse;
+import com.example.rewarded_questions_app.dto.response.QuestionDTO;
 import com.example.rewarded_questions_app.dto.response.QuestionnaireDTO;
 import com.example.rewarded_questions_app.exceptions.EntityInvalidArgumentException;
 import com.example.rewarded_questions_app.exceptions.EntityNotFoundException;
 import com.example.rewarded_questions_app.exceptions.ValidationException;
+import com.example.rewarded_questions_app.service.QuestionService;
 import com.example.rewarded_questions_app.service.QuestionnaireService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,12 +16,10 @@ import org.jspecify.annotations.NonNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
@@ -26,6 +27,7 @@ import java.security.Principal;
 public class QuestionnaireRestController {
 
     private final QuestionnaireService questionnaireService;
+    private final QuestionService questionService;
 
     @PostMapping()
     public ResponseEntity<@NonNull GenericResponse<QuestionnaireDTO>> createQuestionnaire(
@@ -45,6 +47,31 @@ public class QuestionnaireRestController {
                         questionnaireService.createQuestionnaire(req, principal.getName()),
                         "CreateQuestionnaireSucceeded",
                         "Questionnaire created successfully",
+                        true
+                ),
+                HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{id}/questions")
+    public ResponseEntity<@NonNull GenericResponse<QuestionDTO>> createQuestion(
+            @Valid @RequestBody CreateQuestionRequest req,
+            BindingResult bindingResult,
+            Principal principal,
+            @PathVariable UUID id
+    ) throws ValidationException, EntityInvalidArgumentException, EntityNotFoundException {
+
+        // business validator for create question request
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(error -> {
+                System.out.println(error.getCode() + error.getDefaultMessage());
+            });
+            throw new ValidationException("CreateQuestionRequest", "Question creation failed during validation", bindingResult);
+        }
+        return new ResponseEntity<>(
+                new GenericResponse<>(
+                        questionService.createQuestion(req,  id, principal.getName()),
+                        "CreateQuestionSucceeded",
+                        "Question created successfully",
                         true
                 ),
                 HttpStatus.CREATED);
