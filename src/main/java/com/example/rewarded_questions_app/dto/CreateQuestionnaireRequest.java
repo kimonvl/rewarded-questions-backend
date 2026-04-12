3d1344e05d0fed5e8871808c3defb7aaf3734a35
kimonvl <kimonvloutis@gmail.com>
2026-04-12 03:38:34 +0300

    fix: remove questions

---
+++ src/main/java/com/example/rewarded_questions_app/dto/CreateQuestionnaireRequest.java
@@ -0,0 +1,15 @@
+package com.example.rewarded_questions_app.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateQuestionnaireRequest(
+        @NotBlank
+        @Size(min = 3, max = 200)
+        String title,
+
+        @NotBlank
+        @Size(max = 200)
+        String description
+) {
+}
