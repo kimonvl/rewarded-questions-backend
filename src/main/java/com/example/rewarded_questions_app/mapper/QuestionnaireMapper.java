512a6afc408eb01963eb27c4f04360be08af9720
kimonvl <kimonvloutis@gmail.com>
2026-04-12 03:40:56 +0300

    fix: add create questionnaire request to questionnaire mapper

--- src/main/java/com/example/rewarded_questions_app/mapper/QuestionnaireMapper.java
+++ src/main/java/com/example/rewarded_questions_app/mapper/QuestionnaireMapper.java
@@ -1,7 +1,9 @@
 package com.example.rewarded_questions_app.mapper;
 
+import com.example.rewarded_questions_app.dto.CreateQuestionnaireRequest;
 import com.example.rewarded_questions_app.dto.response.QuestionnaireDTO;
 import com.example.rewarded_questions_app.model.questionnaire.Questionnaire;
+import com.example.rewarded_questions_app.model.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,15 @@ import org.springframework.stereotype.Component;
 public class QuestionnaireMapper {
 
     private final QuestionMapper questionMapper;
+
+    public Questionnaire createQuestionnaireReqToQuestionnaire(CreateQuestionnaireRequest request, User user) {
+        //TODO: builder pattern
+        Questionnaire questionnaire = new Questionnaire();
+        questionnaire.setTitle(request.title());
+        questionnaire.setDescription(request.description());
+        questionnaire.setUser(user);
+        return questionnaire;
+    }
 
     public QuestionnaireDTO toQuestionnaireDTO(Questionnaire questionnaire) {
         return new QuestionnaireDTO(
