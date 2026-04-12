0d45b1955e90ac75fc054f8452a329665b132d76
kimonvl <kimonvloutis@gmail.com>
2026-04-12 03:42:03 +0300

    fix: add exists by user id and title

--- src/main/java/com/example/rewarded_questions_app/repository/QuestionnaireRepository.java
+++ src/main/java/com/example/rewarded_questions_app/repository/QuestionnaireRepository.java
@@ -4,4 +4,5 @@ import com.example.rewarded_questions_app.model.questionnaire.Questionnaire;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionnaireRepository extends JpaRepository<Questionnaire, Long> {
+    boolean existsByUserIdAndTitle(Long id, String title);
 }
