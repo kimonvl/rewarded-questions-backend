c990b6a8bd87fc7cf623b2e698e7757f1523673c
kimonvl <kimonvloutis@gmail.com>
2026-04-12 03:39:32 +0300

    fix: replace username with email

--- src/main/java/com/example/rewarded_questions_app/dto/LoginRequest.java
+++ src/main/java/com/example/rewarded_questions_app/dto/LoginRequest.java
@@ -1,9 +1,11 @@
 package com.example.rewarded_questions_app.dto;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
-        @NotBlank String username,
-        @NotBlank String password,
+        @NotBlank @Email @Size(min = 3, max = 100) String email,
+        @NotBlank @Size(min = 6, max = 100) String password,
         @NotNull Long roleId
 ) {}
