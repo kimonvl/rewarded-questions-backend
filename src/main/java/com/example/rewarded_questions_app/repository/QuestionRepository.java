c6efb76b4d64ef16022600fee7e21629e2495a00
kimonvl <kimonvloutis@gmail.com>
2026-04-13 02:22:13 +0300

    chore: add existsByTextAndQuestionnaireId

--- src/main/java/com/example/rewarded_questions_app/repository/QuestionRepository.java
+++ src/main/java/com/example/rewarded_questions_app/repository/QuestionRepository.java
@@ -3,5 +3,8 @@ package com.example.rewarded_questions_app.repository;
 import com.example.rewarded_questions_app.model.questionnaire.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    boolean existsByTextAndQuestionnaireId(String text, Long questionnaireId);
 }
