504d30fccc325483e3935a3a3ce907990e1459d9
kimonvl <kimonvloutis@gmail.com>
2026-04-19 00:57:05 +0300

    chore: add findQuestionnaireByUuid

--- src/main/java/com/example/rewarded_questions_app/service/QuestionnaireService.java
+++ src/main/java/com/example/rewarded_questions_app/service/QuestionnaireService.java
@@ -4,7 +4,12 @@ import com.example.rewarded_questions_app.dto.CreateQuestionnaireRequest;
 import com.example.rewarded_questions_app.dto.response.QuestionnaireDTO;
 import com.example.rewarded_questions_app.exceptions.EntityInvalidArgumentException;
 import com.example.rewarded_questions_app.exceptions.EntityNotFoundException;
+import com.example.rewarded_questions_app.model.questionnaire.Questionnaire;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface QuestionnaireService {
     QuestionnaireDTO createQuestionnaire(CreateQuestionnaireRequest request, String email) throws EntityNotFoundException, EntityInvalidArgumentException;
+    Optional<Questionnaire> findQuestionnaireByUuid(UUID uuid);
 }
