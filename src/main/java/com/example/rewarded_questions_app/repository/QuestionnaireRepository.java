655fa414ddb4a84f66193c68bba6150588d8ead0
kimonvl <kimonvloutis@gmail.com>
2026-04-20 14:08:38 +0300

    chore: add findWithQuestionsByUuid with graph

--- src/main/java/com/example/rewarded_questions_app/repository/QuestionnaireRepository.java
+++ src/main/java/com/example/rewarded_questions_app/repository/QuestionnaireRepository.java
@@ -1,6 +1,7 @@
 package com.example.rewarded_questions_app.repository;
 
 import com.example.rewarded_questions_app.model.questionnaire.Questionnaire;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,6 +10,8 @@ import java.util.UUID;
 public interface QuestionnaireRepository extends JpaRepository<Questionnaire, Long> {
     boolean existsByUserIdAndTitle(Long id, String title);
 
+    @EntityGraph(attributePaths = "questions")
+    Optional<Questionnaire> findWithQuestionsByUuid(UUID uuid);
     Optional<Questionnaire> findByUuid(UUID uuid);
     Optional<Questionnaire> findByUserIdAndTitle(Long userId, String title);
 }
