24af7f553b7050dfdf0989b21597d9605b937eef
kimonvl <kimonvloutis@gmail.com>
2026-04-23 00:42:36 +0300

    chore: add edit questionnaire details service

--- src/main/java/com/example/rewarded_questions_app/service/QuestionnaireServiceImpl.java
+++ src/main/java/com/example/rewarded_questions_app/service/QuestionnaireServiceImpl.java
@@ -44,7 +44,7 @@ public class QuestionnaireServiceImpl implements QuestionnaireService{
 
             Questionnaire saved = questionnaireRepository.save(questionnaireMapper.createQuestionnaireReqToQuestionnaire(request, user));
             log.info("Questionnaire created successfully for email={}", email);
-            return questionnaireMapper.toQuestionnaireDTO(saved);
+            return questionnaireMapper.toQuestionnaireWithQuestionsDTO(saved);
         } catch (EntityNotFoundException | EntityInvalidArgumentException e) {
             log.warn("Questionnaire creation failed. Message={}", e.getMessage());
             throw e;
@@ -53,9 +53,48 @@ public class QuestionnaireServiceImpl implements QuestionnaireService{
 
     @Override
     @PreAuthorize("hasAuthority('EDIT_QUESTIONNAIRE')")
-    @Transactional(rollbackFor = {})
-    public QuestionnaireDetailsDTO editQuestionnaireDetails(EditQuestionnaireDetailsRequest request, UUID questionnaireId, String email) {
-        return null;
+    @Transactional(rollbackFor = {EntityNotFoundException.class, EntityInvalidArgumentException.class})
+    public QuestionnaireDetailsDTO editQuestionnaireDetails(EditQuestionnaireDetailsRequest request, UUID questionnaireId, String email) throws EntityNotFoundException, EntityInvalidArgumentException {
+        try {
+            User user = userRepository.findByEmail(email)
+                    .orElseThrow(() -> new EntityNotFoundException("EditQuestionnaireDetailsUser", "User with email=" + email + " not found"));
+            Questionnaire questionnaire = questionnaireRepository.findByUuid(questionnaireId)
+                    .orElseThrow(() -> new EntityNotFoundException("EditQuestionnaireDetailsQuestionnaire", "Questionnaire with id=" + questionnaireId + " not found"));
+            if(!questionnaire.getUser().equals(user)) {
+                throw new EntityInvalidArgumentException("EditQuestionnaireDetailsUserQuestionnaire", "User with email=" + email + " is not the owner of the questionnaire with id=" + questionnaireId);
+            }
+            if (request.title() == null && request.description() == null) {
+                throw new EntityInvalidArgumentException("EditQuestionnaireDetailsBothFieldsNull", "At least one of the fields (title or description) must be provided for editing.");
+            }
+            if (request.title() != null && request.title().trim().isBlank()) {
+                throw new EntityInvalidArgumentException("EditQuestionnaireDetailsBlankTitle", "Blank title provided for editing.");
+            }
+            if (request.description() != null && request.description().trim().isBlank()) {
+                throw new EntityInvalidArgumentException("EditQuestionnaireDetailsBlankDescription", "Blank description provided for editing.");
+            }
+            if(request.title() != null) {
+                if (request.title().trim().length() < 3 || request.title().trim().length() > 30) {
+                    throw new EntityInvalidArgumentException("EditQuestionnaireDetailsTitleLength", "Title length must be between 3 and 30 characters.");
+                }
+                if (questionnaireRepository.existsByUserIdAndTitle(user.getId(), request.title().trim()) && !questionnaire.getTitle().equals(request.title())) {
+                    throw new EntityInvalidArgumentException("EditQuestionnaireDetailsTitleNotUnique", "Questionnaire with title=" + request.title() + " already exists for user with email=" + email);
+                }
+                questionnaire.setTitle(request.title().trim());
+            }
+            if(request.description() != null) {
+                if (request.description().trim().length() < 5 || request.description().trim().length() > 200) {
+                    throw new EntityInvalidArgumentException("EditQuestionnaireDetailsDescriptionsLength", "Descriptions length must be between 3 and 30 characters.");
+                }
+                questionnaire.setDescription(request.description().trim());
+            }
+
+            QuestionnaireDetailsDTO questionnaireDetailsDTO = questionnaireMapper.toQuestionnaireDetailsDTO(questionnaireRepository.save(questionnaire));
+            log.info("Questionnaire details with id={} edited successfully by user with email={}",questionnaireId, email);
+            return questionnaireDetailsDTO;
+        } catch (EntityNotFoundException | EntityInvalidArgumentException e) {
+            log.warn("Questionnaire details edit failed. Message={}", e.getMessage());
+            throw e;
+        }
     }
 
     @Override
