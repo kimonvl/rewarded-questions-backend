f18de8ba568f1835892ef436469cad034d6e28b7
kimonvl <kimonvloutis@gmail.com>
2026-04-25 01:23:47 +0300

    chore: add findByUuidAndDeletedFalse loads possible choices

--- src/main/java/com/example/rewarded_questions_app/repository/QuestionRepository.java
+++ src/main/java/com/example/rewarded_questions_app/repository/QuestionRepository.java
@@ -1,8 +1,15 @@
 package com.example.rewarded_questions_app.repository;
 
 import com.example.rewarded_questions_app.model.questionnaire.Question;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    @EntityGraph(attributePaths = {"possibleChoices"})
+    Optional<Question> findByUuidAndDeletedFalse(UUID uuid);
+
     boolean existsByTextAndQuestionnaireIdAndDeletedFalse(String text, Long questionnaireId);
 }
