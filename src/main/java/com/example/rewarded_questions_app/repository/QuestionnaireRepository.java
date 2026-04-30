90a809a7c93d9b0b6b96fa0701b1af790e4e48b0
kimonvl <kimonvloutis@gmail.com>
2026-04-30 13:27:51 +0300

    chore: load user with questionnaire in findAll with pagination

--- src/main/java/com/example/rewarded_questions_app/repository/QuestionnaireRepository.java
+++ src/main/java/com/example/rewarded_questions_app/repository/QuestionnaireRepository.java
@@ -1,13 +1,19 @@
 package com.example.rewarded_questions_app.repository;
 
 import com.example.rewarded_questions_app.model.questionnaire.Questionnaire;
+import jakarta.annotation.Nullable;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface QuestionnaireRepository extends JpaRepository<Questionnaire, Long> {
+public interface QuestionnaireRepository extends JpaRepository<Questionnaire, Long>, JpaSpecificationExecutor<Questionnaire> {
     boolean existsByUserIdAndTitleAndDeletedFalse(Long id, String title);
     boolean existsByUserEmailAndTitleAndDeletedFalse(String email, String title);
 
@@ -18,4 +24,10 @@ public interface QuestionnaireRepository extends JpaRepository<Questionnaire, Lo
     Optional<Questionnaire> findWithQuestionsByUuidAndDeletedFalse(UUID uuid);
     Optional<Questionnaire> findByUuidAndDeletedFalse(UUID uuid);
     Optional<Questionnaire> findByUserIdAndTitleAndDeletedFalse(Long userId, String title);
+
+    @Override
+    @NullMarked
+    @EntityGraph(attributePaths = "user")
+    Page<Questionnaire> findAll(@Nullable Specification<Questionnaire> spec, Pageable pageable);
+
 }
